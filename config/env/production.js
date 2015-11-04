59564708321b6aa0bc67ba6172898d6731992742
Mullen Tony <tony.mullen@gmail.com>
2015-11-03 22:13:41 -0800

    changed mongodb login

--- config/env/production.js
+++ config/env/production.js
@@ -10,7 +10,7 @@ module.exports = {
   port: process.env.PORT || 8443,
   db: {
 //    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/automata',
-    uri: 'mongodb://tony:mongo*pass@ec2-54-152-150-143.compute-1.amazonaws.com:27017/dummyDB',
+    uri: 'mongodb://remote:mongo*pass@ec2-54-152-150-143.compute-1.amazonaws.com:27017/dummyDB',
     options: {
       user: '',
       pass: ''
